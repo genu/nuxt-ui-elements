a96c93225a3be16aab4ed4492a14ba7c7d1da7c7
Eugen Istoc <eugenistoc@gmail.com>
2026-01-06 08:44:23 -0500

    feat: add slugify utility

--- src/runtime/utils/std.ts
+++ src/runtime/utils/std.ts
@@ -3,14 +3,16 @@
  * Import from '#std' for tree-shakeable utilities
  *
  * @example
- * import { plur, date } from '#std'
+ * import { plur, slugify, date } from '#std'
  *
  * const text = plur('item', count)
+ * const slug = slugify('Hello World')
  * const nextMonth = date.add(date.today(), 1, 'month')
  */
 
 // String utilities
 export { default as plur } from "plur"
+export { default as slugify } from "@sindresorhus/slugify"
 
 // Date utilities - all functions exported as namespace
 export * as date from "./std/date"
